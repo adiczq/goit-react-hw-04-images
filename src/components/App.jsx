c65c49924332f732af61fd285deb384e12caca14
Adrian Lacheta <adiczq@gmail.com>
2023-07-11 16:29:27 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,122 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+import Button from './Button/Button';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
+import css from './App.module.css';
+import { toast } from 'react-toastify';
+
+class App extends Component {
+  state = {
+    images: [],
+    isLoading: false,
+    error: null,
+    query: '',
+    page: 1,
+    showModal: false,
+    selectedImage: null,
+    isLastPage: false,
+  };
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.setState({ images: [], page: 1, isLastPage: false }, () => {
+        this.fetchImages();
+      });
+    }
+  }
+
+  fetchImages = () => {
+    const { query, page } = this.state;
+    const API_KEY = '36285780-5e432e43a01ab0bbeda1983f2';
+
+    this.setState({ isLoading: true });
+
+    axios
+      .get(
+        `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      )
+      .then(response => {
+        const { hits, totalHits } = response.data;
+
+        if (hits.length === 0) {
+          return toast('Sorry, there are no images matching your request...', {
+            position: toast.POSITION.TOP_CENTER,
+          });
+        }
+
+        const modifiedHits = hits.map(
+          ({ id, tags, webformatURL, largeImageURL }) => ({
+            id,
+            tags,
+            webformatURL,
+            largeImageURL,
+          })
+        );
+
+        this.setState(prevState => ({
+          images: [...prevState.images, ...modifiedHits],
+          page: prevState.page + 1,
+          isLastPage:
+            prevState.images.length + modifiedHits.length >= totalHits,
+        }));
+      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      })
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  };
+
+  handleSearchSubmit = query => {
+    if (this.state.query === query) {
+      return;
+    }
+    this.setState({
+      query: query,
+      page: 1,
+      images: [],
+      error: null,
+      isLastPage: false,
+    });
+  };
+
+  handleImageClick = image => {
+    this.setState({ selectedImage: image, showModal: true });
+    document.body.style.overflow = 'hidden';
+  };
+
+  handleModalClose = () => {
+    this.setState({ selectedImage: null, showModal: false });
+    document.body.style.overflow = 'auto';
+  };
+
+  render() {
+    const { images, isLoading, error, showModal, selectedImage, isLastPage } =
+      this.state;
+    return (
+      <div className={css.App}>
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+
+        {error && <p>Error: {error}</p>}
+
+        <ImageGallery images={images} onItemClick={this.handleImageClick} />
+
+        {isLoading && <Loader />}
+
+        {!isLoading && images.length > 0 && !isLastPage && (
+          <Button onClick={this.fetchImages} />
+        )}
+
+        {showModal && (
+          <Modal image={selectedImage} onClose={this.handleModalClose} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
