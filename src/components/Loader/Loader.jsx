c74e86616dd357fbc9e24995a699599b16bd4138
Adrian Lacheta <adiczq@gmail.com>
2023-07-25 16:58:19 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,23 +1,21 @@
-import { Component } from 'react';
+import React from 'react';
 import { Circles } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className={css.Container}>
-        <Circles
-          height="80"
-          width="80"
-          color="#3f51b5"
-          ariaLabel="circles-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          visible={true}
-        />
-      </div>
-    );
-  }
-}
+const Loader = () => {
+  return (
+    <div className={css.Container}>
+      <Circles
+        height={80}
+        width={80}
+        color="#3f51b5"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+};
 
 export default Loader;
