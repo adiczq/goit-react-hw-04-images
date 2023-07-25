754bdcf93a8bf992411baf691e2ff596727f5030
Adrian Lacheta <adiczq@gmail.com>
2023-07-25 16:58:26 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Button.module.css';
 
-class Button extends Component {
-  render() {
-    const { onClick } = this.props;
-    return (
-      <div className={css.Container}>
-        <button className={css.Button} onClick={onClick}>
-          Load More
-        </button>
-      </div>
-    );
-  }
-}
+const Button = ({ onClick }) => {
+  return (
+    <div className={css.Container}>
+      <button className={css.Button} onClick={onClick}>
+        Load More
+      </button>
+    </div>
+  );
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
